a9501d9d83ae2f8095a19d9eaf5294b9f5840b74
Yulms <yminsk@gmail.com>
2020-05-27 09:49:15 +0300

    Добавляет js модальных окон

--- source/js/app.js
+++ source/js/app.js
@@ -6,7 +6,7 @@ var navToggle = document.querySelector(".header__nav-toggle");
 function menuToggle() {
   nav.classList.toggle("header--nav-opened");
   nav.classList.toggle("header--nav-closed");
-}
+};
 
 if (nav && navToggle) {
   nav.classList.remove("header--nojs");
@@ -16,10 +16,81 @@ if (nav && navToggle) {
     evt.preventDefault();
     menuToggle()
   });
-}
+};
 
-/*
- */
+
+
+
+
+
+
+
+
+
+
+
+// 0. Пишем код открытия и закрытия модальных окон
+var modalWindow;
+var btnClose;
+
+// Функция открытия модальных окон
+function showModal(evt, modalClass) {
+  modalWindow = document.querySelector(modalClass);
+
+  // Добавляем события закрытия на кнопку закрытия внутри этого модального окна
+  btnClose = document.querySelector(modalClass + " .js--modal-close");
+  btnClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modalWindow.classList.add("modal--closed");
+  });
+
+  // открываем окно
+  modalWindow.classList.remove("modal--closed");
+};
+
+// закрытие окна по нажатию esc
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27 && modalWindow) {
+    modalWindow.classList.add("modal--closed");
+  };
+});
+
+// упрощенная валидация (только наличие инфы в полях)
+var surnameField = document.getElementById("surname-field");
+var nameField = document.getElementById("name-field");
+var phoneField = document.getElementById("phone-field");
+var emailField = document.getElementById("email-field");
+
+function validateFormFields() {
+  if (surnameField.value && nameField.value && phoneField.value && emailField.value) {
+    return true;
+  };
+};
+
+var btnForm = document.querySelector(".button--form-send");
+if (btnForm) {
+
+  btnForm.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    // 1. Проверяем валидность введенных данных
+    if (validateFormFields()) {
+      // 2. Если все верно - отпускаем отправку и показываем окно подтверждения
+      showModal(evt, ".modal--confirmation");
+    } else {
+      // 3. Если нет - модальное окно ошибки
+      showModal(evt, ".modal--error");
+    };
+  });
+};
+
+
+
+
+
+
+
+
+
 // Отложенная загрузка карты
 if (document.querySelector("#map")) {
 
